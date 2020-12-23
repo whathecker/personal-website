dcc330d20f034efacaf26dc356a860939e067431
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-23 20:25:22 +0100

    build: add scaffolding of header component

---
+++ frontend/src/components/shared/header/header.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { StyledHeader } from './styled';
+
+const Header: React.FunctionComponent = () => {
+    return <StyledHeader></StyledHeader>;
+};
+
+export default Header;
---
+++ frontend/src/components/shared/header/styled.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { device } from '../../../utils';
+
+export const StyledHeader = styled.header`
+    width: 100%;
+    height: 70px;
+`;
