5afefba85e54010888eb97f83bd5229ad45bb5d9
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-26 20:28:46 +0100

    build: minor style change at header component

--- frontend/src/components/shared/header/styled.tsx
+++ frontend/src/components/shared/header/styled.tsx
@@ -5,7 +5,7 @@ export const StyledHeader = styled.header`
   display: flex;
   align-items: center;
   width: 100%;
-  height: 120px;
+  height: 100px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding-left: 30px;
 `;
@@ -21,8 +21,7 @@ export const StyledMenuWrapper = styled.div`
   height: 60px;
   align-items: center;
   margin-left: auto;
-  margin-right: 60px;
+  margin-right: 50px;
   padding-left: 15px;
   padding-right: 15px;
-  border: 1px solid black;
 `;
