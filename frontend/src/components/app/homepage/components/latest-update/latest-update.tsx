bfc516431849b6fbf0685413bad03b4abfd54881
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-27 20:47:39 +0100

    refactor: make currently-reading as nested component of latest-update

--- frontend/src/components/app/homepage/components/latest-update/latest-update.tsx
+++ frontend/src/components/app/homepage/components/latest-update/latest-update.tsx
@@ -1,47 +1,10 @@
 import * as React from 'react';
-import {
-  CurrentlyReadingSection,
-  HeaderWrapper,
-  CurrentlyReadingHeader,
-  CurrentlyReadingLink,
-  BookWrapper,
-  BookImgWrapper,
-  BookImg,
-  BookImgLink,
-  BookDetailWrapper,
-  BookTitle,
-  BookTitleLink,
-  AuthorInfo,
-} from './styled';
+import { CurrentlyReading } from './components';
 
 const LatestUpdate: React.FunctionComponent = () => {
   return (
     <>
-      <CurrentlyReadingSection>
-        <HeaderWrapper>
-          <CurrentlyReadingHeader>{`Currently Reading`}</CurrentlyReadingHeader>
-          <CurrentlyReadingLink>{`checkout my reading list`}</CurrentlyReadingLink>
-        </HeaderWrapper>
-        <BookWrapper>
-          <BookImgWrapper>
-            <BookImgLink
-              href="https://www.amazon.com/Domain-Driven-Design-Tackling-Complexity-Software/dp/0321125215/ref=sr_1_1?dchild=1&keywords=domain+driven+design&qid=1609097175&sr=8-1"
-              target="_blank"
-            >
-              <BookImg src="/img/example-book-img.jpg" />
-            </BookImgLink>
-          </BookImgWrapper>
-          <BookDetailWrapper>
-            <BookTitleLink
-              href="https://www.amazon.com/Domain-Driven-Design-Tackling-Complexity-Software/dp/0321125215/ref=sr_1_1?dchild=1&keywords=domain+driven+design&qid=1609097175&sr=8-1"
-              target="_blank"
-            >
-              <BookTitle>{`Domain Driven Design: Tackling Complexity in the Heart of Software`}</BookTitle>
-            </BookTitleLink>
-            <AuthorInfo>{`by Eric Evans`}</AuthorInfo>
-          </BookDetailWrapper>
-        </BookWrapper>
-      </CurrentlyReadingSection>
+      <CurrentlyReading />
     </>
   );
 };
