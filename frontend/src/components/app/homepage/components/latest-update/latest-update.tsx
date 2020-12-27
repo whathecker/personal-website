1c80a6d54ff022aa19988bb3d28b677b2128385d
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-27 20:05:14 +0100

    build: add currently reading section in lastest-update component

--- frontend/src/components/app/homepage/components/latest-update/latest-update.tsx
+++ frontend/src/components/app/homepage/components/latest-update/latest-update.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
-import { CurrentlyReadingSection } from './styled';
+import {
+  CurrentlyReadingSection,
+  HeaderWrapper,
+  CurrentlyReadingHeader,
+  CurrentlyReadingLink,
+  BookWrapper,
+  BookImgWrapper,
+  BookImg,
+  BookDetailWrapper,
+  BookTitle,
+  AuthorInfo,
+} from './styled';
 
 const LatestUpdate: React.FunctionComponent = () => {
   return (
     <>
-      <CurrentlyReadingSection>Currently reading</CurrentlyReadingSection>
+      <CurrentlyReadingSection>
+        <HeaderWrapper>
+          <CurrentlyReadingHeader>{`Currently Reading`}</CurrentlyReadingHeader>
+          <CurrentlyReadingLink>{`checkout my reading list`}</CurrentlyReadingLink>
+        </HeaderWrapper>
+        <BookWrapper>
+          <BookImgWrapper>
+            <BookImg src="/img/example-book-img.jpg" />
+          </BookImgWrapper>
+          <BookDetailWrapper>
+            <BookTitle>{`Domain Driven Design: Tackling Complexity in the Heart of Software`}</BookTitle>
+            <AuthorInfo>{`by Eric Evans`}</AuthorInfo>
+          </BookDetailWrapper>
+        </BookWrapper>
+      </CurrentlyReadingSection>
     </>
   );
 };
--- frontend/src/components/app/homepage/components/latest-update/styled.tsx
+++ frontend/src/components/app/homepage/components/latest-update/styled.tsx
@@ -5,4 +5,62 @@ export const CurrentlyReadingSection = styled.div`
   width: 100%;
   height: auto;
   border: 1px solid orange;
+  padding-top: 10px;
+`;
+
+export const HeaderWrapper = styled.div`
+  height: auto;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+export const CurrentlyReadingHeader = styled.h2`
+  display: inline;
+  font-weight: bold;
+  font-size: 1.1em;
+  padding-left: 15px;
+`;
+
+export const CurrentlyReadingLink = styled.a`
+  display: inline;
+  color: #072ac8;
+  font-size: 0.9em;
+  padding-left: 25px;
+  cursor: pointer;
+`;
+
+export const BookWrapper = styled.div`
+  height: auto;
+  padding-top: 20px;
+  padding-left: 25px;
+  padding-bottom: 20px;
+`;
+
+export const BookImgWrapper = styled.div`
+  display: inline-block;
+  width: 120px;
+  height: 140px;
+`;
+
+export const BookImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const BookDetailWrapper = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  padding-top: 16px;
+  padding-left: 25px;
+`;
+
+export const BookTitle = styled.h4`
+  font-weight: bold;
+  font-size: 0.9em;
+`;
+
+export const AuthorInfo = styled.p`
+  font-size: 0.8em;
+  padding-top: 10px;
+  padding-left: 1px;
 `;
