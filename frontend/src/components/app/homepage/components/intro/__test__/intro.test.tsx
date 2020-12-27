0ed89ce976a699b3cca8e39159521b1590055256
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-27 16:30:55 +0100

    chore: add snapshot test of intro component

---
+++ frontend/src/components/app/homepage/components/intro/__test__/intro.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Intro from '../intro';
+import { shallow } from 'enzyme';
+
+describe('Test Intro component', () => {
+    it('should renders correctly', () => {
+      const tree = shallow(<Intro />);
+      expect(tree).toMatchSnapshot();
+    });
+  });
