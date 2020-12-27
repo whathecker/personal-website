c38ce00aff6924bc22a9bed66dd23f745dd36e94
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-27 21:09:34 +0100

    build: add reading-list as new route

--- frontend/route.js
+++ frontend/route.js
@@ -2,6 +2,7 @@ const routes = require('next-routes');
 
 module.exports = routes()
 .add('/', 'index')
+.add('/reading-list', 'reading-list')
 
 
 
---
+++ frontend/src/pages/reading-list.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Header, Footer } from '../components/shared';
+
+const ReadingList: React.FunctionComponent = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default ReadingList;
