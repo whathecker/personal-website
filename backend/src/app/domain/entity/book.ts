e1d98aac173b890fbc7c6e5bf50be7cacabbf666
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2020-12-28 22:06:31 +0100

    build: change data type of readStatus field to enum ReadStatus

--- backend/src/app/domain/entity/book.ts
+++ backend/src/app/domain/entity/book.ts
@@ -1,11 +1,16 @@
 import { ID } from '../definition/id';
 import { IEntity } from './entity';
 
+export enum ReadStatus {
+  reading = 'READING',
+  finished = 'FINISHED'
+}
+
 export class Book implements IEntity {
   constructor(
     public title: string,
     public author: string,
-    public readStatus: string, // Change it to enum?
+    public readStatus: ReadStatus,
     public coverImage: string,
     public review?: string,
     public rating?: number,
